b5f70176cb2fe76d965ada63854a4d52fecacd25
Andreas <Andreas@ANDREAS-SSD.lan>
2012-04-28 10:51:21 +0100

    Remove useless enum JLabelName

--- software/tof/ToF/src/trampoline/TrampolineUI.java
+++ software/tof/ToF/src/trampoline/TrampolineUI.java
@@ -413,10 +413,6 @@ public class TrampolineUI extends javax.swing.JFrame {
         beamStatusTimer.stop();
     }
     
-    public enum JLabelName {
-        N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, TOF, TON, TOTAL, ALL;
-    }
-    
     //This function updates the mini chart on each bounce. 
     public void updateChart(double[] values, String[] names, String title, JPanel pan) {        
         chartObject_.updateInfo(values, names, title);
