fbaa3462dbad104201fc082b7012005ec52f3965
Andreas <Andreas@ANDREAS-SSD.lan>
2012-04-29 20:57:59 +0100

    Add ComboItem.java, to display different values/names in combo boxes.

---
+++ software/tof/ToF/src/trampoline/ComboItem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trampoline;
+
+/**
+ *
+ * @author Andreas
+ */
+public class ComboItem {
+    private String value;
+    private String label;
+
+    public void ComboValue(String value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public String toString() {
+        return this.label;
+    }
+}
