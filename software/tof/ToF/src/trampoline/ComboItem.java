ad9110dec34c73f38ac8ddfc6a6107faf8172495
Andreas <Andreas@ANDREAS-SSD.lan>
2012-05-03 20:29:59 +0100

    Add overrides to equals() and hashcode() for the ComboItem so we can compare them.

--- software/tof/ToF/src/trampoline/ComboItem.java
+++ software/tof/ToF/src/trampoline/ComboItem.java
@@ -9,34 +9,55 @@ package trampoline;
  * @author Andreas
  */
 public class ComboItem {
-    private String id;
-    private String name;
+    private String id_;
+    private String name_;
 
     public ComboItem(String id, String name) {
-        this.id = id;
-        this.name = name;
+        this.id_ = id;
+        this.name_ = name;
     }
 
     public ComboItem(int id, String name) {
-        this.id = Integer.toString(id);
-        this.name = name;
+        this.id_ = Integer.toString(id);
+        this.name_ = name;
+    }
+    
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+            return true;
+        if((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        // object must be Test at this point
+        ComboItem otherItem = (ComboItem)obj;
+        return name_ == otherItem.getName();
+    }
+
+    @Override
+    public int hashCode() {
+        final int seed = 37;
+        int result = 1;
+        result = seed * result + ((name_ == null) ? 0 : name_.hashCode());
+        result = seed * result + Integer.parseInt(id_);
+        return result;
     }
 
     public String getID() {
-        return this.id;
+        return this.id_;
     }
 
     //Shortcut only
     public int getNumericID() {
-        return Integer.parseInt(this.id);
+        return Integer.parseInt(this.id_);
     }
 
     public String getName() {
-        return this.name;
+        return this.name_;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name_;
     }
 }
