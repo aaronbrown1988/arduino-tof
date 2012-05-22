937ead5cd1202c5ee52e3a37776af4317d7673dc
Kieran <Kieran@Kieryb.lan>
2012-05-22 11:24:16 +0100

    Added in error Colours (so we can display multiple sorts of messages not just errors)

--- software/tof/ToF/src/trampoline/ErrorHandler.java
+++ software/tof/ToF/src/trampoline/ErrorHandler.java
@@ -13,19 +13,37 @@ public class ErrorHandler {
     private boolean flag_;   // Flag if there is an error
     private String currentErrorShort_; //Short version for display of current error;
     private String currentErrorLong_; //Long version of current error;
-    private String moreDetails_;
+    private java.awt.Color currentColour_; // Colour of current error;
+    private String moreDetails_; // Debugging details 
     
-    private static final String[] shortErrors_ =   {"","Error: Incorrect password supplied for club management tab",
+    
+    private static final java.awt.Color[] errorColour_ = {null, new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(0,0,255),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                          new java.awt.Color(255,0,0),
+                                                         };
+    
+    private static final String[] shortErrors_ =   {"","Error: Incorrect password",
                                                     "Error: Database connection error",
                                                     "Error: ToF not attached",
                                                     "Logout Successful",
-                                                    "Error: Comm port settings file not located",
-                                                    "Error: General I/O error with a file",
-                                                    "Error: Problem completing handshake with ToF",
-                                                    "Error: Problem communicating with ToF device",
-                                                    "Error: Problem with clearing cache on COM Port",
-                                                    "Error: Problem with access the SQL database"
+                                                    "Error: Settings file",
+                                                    "Error: General file I/O",
+                                                    "Error: Handshake failed",
+                                                    "Error: Device Communications",
+                                                    "Error: COM Port Cache",
+                                                    "Error: Database Access",
+                                                    "Error: No tag name"
                                                    };
+    
+    
     
     private static final String[] longErrors_ =    {"","The password supplied for access to the Club Managment tab was incorrect. Please try again.",
                                                     "An error occured whilst trying to initialise the database connection.",
@@ -36,7 +54,8 @@ public class ErrorHandler {
                                                     "The handshake with one of the ToF devices or COM ports on the system was not completed correctly. Please reboot all attached ToF devices.",
                                                     "There was an error communicating with the ToF device. Please reboot all attached ToF devices.",
                                                     "There was an issue with emptying the COM port data cache. Please restart the computer and reboot all attached ToF devices.",
-                                                    "An issue was encountered whilst reading from the SQL database. Please ensure that only one copy of the program is open and try restarting the computer if the problem persists."
+                                                    "An issue was encountered whilst reading from the SQL database. Please ensure that only one copy of the program is open and try restarting the computer if the problem persists.",
+                                                    "The new tag was not added to the database because no tag name was provided or it was the same as an existing tag name. Please try again and input a different tag name."
                                                    };
     
     ErrorHandler(){
@@ -44,6 +63,7 @@ public class ErrorHandler {
         this.currentErrorShort_ = "";
         this.currentErrorLong_ = "";
         this.moreDetails_ = "";
+        this.currentColour_ = null;
     }
     
     boolean isError(){
@@ -61,11 +81,16 @@ public class ErrorHandler {
     String getMoreDetails(){
         return this.moreDetails_;
     }
+    
+    java.awt.Color getColour(){
+        return this.currentColour_;
+    }
             
     void setError(int errno){
         this.flag_ = true;
         this.currentErrorShort_ = this.shortErrors_[errno];
         this.currentErrorLong_ = this.longErrors_[errno];
+        this.currentColour_ = this.errorColour_[errno];
         this.moreDetails_ = "";
     }
     
